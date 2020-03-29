36f1afd402dfbeb13ed3aff8e6d2ebce43e2476e
tomBateup <62841971+tomBateup@users.noreply.github.com>
2020-03-29 17:31:44 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(100,100);
+}
+
+function draw() {
+  noFill();
+  let x1 = 85,
+   x2 = 10,
+   x3 = 90,
+   x4 = 15;
+  let y1 = 20,
+   y2 = 10,
+   y3 = 90,
+   y4 = 80;
+  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+  fill(255);
+  let steps = 10;
+  for (let i = 0; i <= steps; i++) {
+    let t = i / steps;
+    let x = bezierPoint(x1, x2, x3, x4, t);
+    let y = bezierPoint(y1, y2, y3, y4, t);
+    ellipse(x, y, 5, 5);
+  }
+}
+
